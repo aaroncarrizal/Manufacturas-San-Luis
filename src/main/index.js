2cda4cf8ad071719801a12461e6ae9b9bf263bcd
Aarón Carrizal <180816@upslp.edu.mx>
2023-09-26 14:17:39 -0600

    changed window size

--- src/main/index.js
+++ src/main/index.js
@@ -7,8 +7,8 @@ import backend from './backend/main'
 function createWindow() {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 900,
-    height: 670,
+    width: 850,
+    height: 900,
     show: false,
     autoHideMenuBar: true,
     ...(process.platform === 'linux' ? { icon } : { icon }),
