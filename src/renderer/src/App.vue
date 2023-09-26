c4b00cf02f4167a7dfa705d871eacddec4e4b03a
Aarón Carrizal <180816@upslp.edu.mx>
2023-09-26 14:17:27 -0600

    moved navbar to own component

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -1,50 +1,12 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import Navbar from './components/Navbar.vue';
 </script>
-
 <template>
-    <header class="mb-3">
-        <div class="wrapper">
-            <nav class="navbar navbar-expand-lg bg-body-tertiary">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="/parts">
-                        <img src="./assets/icon.jpeg" alt="Bootstrap" width="30" height="30">
-                    </a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                        data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false"
-                        aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                        <ul class="navbar-nav">
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                                    aria-expanded="false">
-                                    Partes
-                                </a>
-                                <ul class="dropdown-menu">
-                                    <li><RouterLink class="nav-link" to="/parts">Ver lista</RouterLink></li>
-                                    <li><RouterLink class="nav-link" to="/parts/new">Registrar nueva</RouterLink></li>
-                                </ul>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                                    aria-expanded="false">
-                                    Fichas
-                                </a>
-                                <ul class="dropdown-menu">
-                                    <li><RouterLink class="nav-link" to="/tokens">Ver lista</RouterLink></li>
-                                    <li><RouterLink class="nav-link" to="/tokens/new">Registrar</RouterLink></li>
-                                </ul>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-        </div>
-    </header>
+    <div>
+        <Navbar></Navbar>
+        <router-view></router-view>
 
-    <RouterView />
+    </div>
 </template>
 
 <style>
---
+++ src/renderer/src/components/Navbar.vue
@@ -0,0 +1,50 @@
+<template>
+    <header class="mb-3">
+        <div class="wrapper">
+            <nav class="navbar navbar-expand-lg bg-body-tertiary">
+                <div class="container-fluid">
+                    <router-link class="navbar-brand" to="/parts">
+                        <img src="../assets/icon.jpeg" alt="Bootstrap" width="30" height="30">
+                    </router-link>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false"
+                        aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                        <ul class="navbar-nav">
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                                    aria-expanded="false">
+                                    Partes
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <li><router-link class="nav-link" to="/parts">Ver lista</router-link></li>
+                                    <li><router-link class="nav-link" to="/parts/new">Registrar nueva</router-link></li>
+                                </ul>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                                    aria-expanded="false">
+                                    Fichas
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <li><router-link class="nav-link" to="/tokens">Ver lista</router-link></li>
+                                    <li><router-link class="nav-link" to="/tokens/new">Registrar</router-link></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </header>
+</template>
+<script>
+import { defineComponent } from "vue"
+import router from '../router/index'
+
+export default defineComponent({
+    name: "Nav-bar",
+});
+</script>
